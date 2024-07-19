f882bab7d498950b212bf8ef6dec4f4b30c84a55
Petr Šádek <shetr.git@gmail.com>
2024-07-19 13:53:54 +0200

    web loading text animation

--- www/index.js
+++ www/index.js
@@ -1,3 +1,17 @@
 import init from './target/sandshrew.js'
 
+setInterval(loadingAnimation, 400);
+
+let loadingState = 1;
+let loadingStrings = ["Loading", "Loading.", "Loading..", "Loading...", "Loading..."]
+
+function loadingAnimation() {
+  let loadingDiv = document.getElementById("loading");
+  loadingDiv.innerHTML = loadingStrings[loadingState];
+  loadingState++;
+  if (loadingState >= loadingStrings.length) {
+    loadingState = 0;
+  }
+} 
+
 init()
